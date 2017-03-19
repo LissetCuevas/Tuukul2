19426bfe2f4f7bc6bcef5d4dd2319f3b0d8a217a
Lisset Cuevas <lisset.cuevasj@gmail.com>
2017-03-19 10:02:39 -0600

    UI errors corrections

--- app/src/main/java/com/mx/tuukul/tuukul/ListActivity.java
+++ app/src/main/java/com/mx/tuukul/tuukul/ListActivity.java
@@ -1,11 +1,18 @@
 package com.mx.tuukul.tuukul;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -15,6 +22,18 @@ public class ListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
--- app/src/main/java/com/mx/tuukul/tuukul/SingupActivity.java
+++ app/src/main/java/com/mx/tuukul/tuukul/SingupActivity.java
@@ -2,6 +2,8 @@ package com.mx.tuukul.tuukul;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class SingupActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class SingupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singup);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.Radio_Man:
+                if (checked)
+
+                    break;
+            case R.id.Radio_Woman:
+                if (checked)
+
+                    break;
+        }
     }
 }
